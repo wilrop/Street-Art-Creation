84be067462ab6c027799e82cc98d3d69c65f4154
Willem Röpke <willemropke@hotmail.com>
2020-04-30 15:49:13 +0200

    Save paths to generated images to a CSV file

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@ import argparse
 import torch
 import torch.nn.functional as F
 import torchvision.utils as vutils
+import pandas as pd
 from os import path
 from model import Generator
 
@@ -45,7 +46,7 @@ def generate_images(generator, num, nz, device):
         return images
 
 
-def save(output_dir, images):
+def save(output_dir, images, csv):
     """
     This function will save all the new images to an output directory.
     :param output_dir: The directory to save the images to.
@@ -54,11 +55,21 @@ def save(output_dir, images):
     """
     print("Saving images")
     images = F.interpolate(images, size=SIZE, mode='bicubic')  # The resize operation on tensor.
+    data = {"Path": []}
     for idx, image in enumerate(images):
         name = str(idx) + '.png'
         file_path = path.join(output_dir, name)
         print(image.shape)
         vutils.save_image(image, file_path)
+        data["Path"].append(file_path)
+
+    df = pd.DataFrame(data)
+    if csv is None:
+        name = "artworks.csv"
+        file_path = path.join(output_dir, name)
+        df.to_csv(file_path)
+    else:
+        df.to_csv(csv, mode='a', header=False)
     print("Finished saving images")
 
 
@@ -71,6 +82,7 @@ def generate(args):
     model_path = args.modelPath
     num = args.num
     output_dir = args.outputDir
+    csv = args.artCSV
     ngpu = args.ngpu
     nz = args.nz
     ngf = args.ngf
@@ -79,7 +91,7 @@ def generate(args):
     device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
     generator = load_generator(model_path, ngpu, nz, ngf, nc, device)
     images = generate_images(generator, num, nz, device)
-    save(output_dir, images)
+    save(output_dir, images, csv)
 
 
 if __name__ == '__main__':
@@ -87,6 +99,7 @@ if __name__ == '__main__':
     parser.add_argument('--modelPath', required=True, help='path to the model checkpoint')
     parser.add_argument('--num', type=int, required=True, help='number of artworks to generate')
     parser.add_argument('--outputDir', required=True, help='The directory to save the generated works to')
+    parser.add_argument('--artCSV', help='The current CSV holding all the works and their features')
     parser.add_argument('--nc', type=int, default=3, help='The number of color channels in the input images.')
     parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
     parser.add_argument('--ngf', type=int, default=64)
